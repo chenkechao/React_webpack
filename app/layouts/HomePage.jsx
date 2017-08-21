5d7f9a6b79b78b5477f3fe9a8898c77cd748f81a
chenkechao <chenkechao@zto.cn>
2017-08-21 19:43:52 +0800

    fdsafafasfafasf

---
+++ app/layouts/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+class HomePage extends React.Component {
+    render () {
+        return (
+            <div>
+                Welcome
+            </div>
+        );
+    }
+}
+
+export default HomePage;
